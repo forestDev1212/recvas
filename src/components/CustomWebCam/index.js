8caafbe1a46c746956fe53a84a150022b1dd33d3
forest <alexeidorman@gmail.com>
2024-02-19 07:50:42 -0800

    create mirror state

--- src/components/CustomWebCam/index.js
+++ src/components/CustomWebCam/index.js
@@ -2,31 +2,38 @@ import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 const CustomWebCam = () => {
-  const webcamRef = useRef(null)
-  const [imgSrc, setImgSrc] = useState(null)
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+  const [mirrored, setMirrored] = useState(false)
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
-  }, [webcamRef])
+  }, [webcamRef]);
 
   const retake = () => {
-    setImgSrc(null)
-  }
+    setImgSrc(null);
+  };
   return (
     <div className="container">
-      {
-        imgSrc ? (
-          <img src={imgSrc} alt="webcam" />
-        ) :
-        (
-          <Webcam height={600} width={600}/>
-        )
-      }
-      <div className="btn-container" >
-      <button onClick={capture} >Capture photo</button>
+      {imgSrc ? (
+        <img src={imgSrc} alt="webcam" />
+      ) : (
+        <Webcam height={600} width={600} />
+      )}
+      <div className="controls" >
+        <div>
+          <input
+            type="checkbox"
+            checked={mirrored}
+            onChange={(e) => setMirrored(e.target.checked)}
+          />
+        </div>
+      </div>
+      <div className="btn-container">
+        <button onClick={capture}>Capture photo</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CustomWebCam
+export default CustomWebCam;
